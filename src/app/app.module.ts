c92a3df4ca9c3a84b50ad1e3f3e97ab2cc939192
VictorRosaValdez <victorrosavaldez@hotmail.com>
2022-11-30 23:08:34 +0100

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent} from "./app.component";
+import { ProductComponent} from "./components/product/product/product.component";
+import { OrderComponent} from "./components/order/order/order.component";
+import { NavbarComponent} from "./components/navbar/navbar/navbar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    OrderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
